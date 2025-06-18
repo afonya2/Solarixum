40dd77271ab76946c3837a8bad954382151acaf2
afonya <adamhir@freemail.hu>
2025-06-18 22:56:05 +0200

    begin code

---
+++ index.ts
@@ -0,0 +1,81 @@
+import http from 'http';
+import { WebSocketServer } from 'ws';
+import crypto from 'crypto';
+import url from 'url'
+import fs from 'fs';
+import path from 'path';
+
+const VER = '0.1.0';
+const SERV_NAME = 'Secure Chats Server';
+const PROT_VER = '0.1.0';
+const PROT_NAME = 'Secure Chats Protocol';
+const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+
+function sendResponse(ok: boolean, data: any, error?: string) {
+    let res: any = {
+        ok: ok,
+        body: data
+    }
+    if (!ok) {
+        res.error = error
+    }
+    return JSON.stringify(res)
+}
+
+const httpServer = http.createServer((req, res) => {
+    let clearUrl = req.url?.split('?')[0];
+    let args = url.parse(req.url || "", true).query;
+    if (clearUrl == "/version") {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(sendResponse(true, {
+            protocol: PROT_NAME,
+            protocolVersion: PROT_VER,
+            serverVersion: VER,
+            serverName: SERV_NAME
+        }))
+    } else if (clearUrl == "/client") {
+        res.writeHead(301, { 'Location': '/client/' });
+        res.end();
+    } else if (clearUrl?.startsWith("/client/")) {
+        let pathRemaining = clearUrl.replace("/client/", "");
+        let file = `./client/${path.normalize(pathRemaining)}`;
+        if (file == "./client/.") {
+            file = "./client/index.html";
+        }
+        if (!fs.existsSync(file)) {
+            res.writeHead(404, { 'Content-Type': 'application/json' });
+            res.end(sendResponse(false, null, "Not Found"));
+            return;
+        }
+        let content = fs.readFileSync(file, 'utf8');
+        if (file.endsWith(".html")) {
+            res.writeHead(200, { 'Content-Type': 'text/html', 'cache-control': 'max-age=86400' });
+            res.end(content);
+        } else if (file.endsWith(".js")) {
+            res.writeHead(200, { 'Content-Type': 'application/javascript', 'cache-control': 'max-age=86400' });
+            res.end(content);
+        } else if (file.endsWith(".css")) {
+            res.writeHead(200, { 'Content-Type': 'text/css', 'cache-control': 'max-age=86400' });
+            res.end(content);
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/text', 'cache-control': 'max-age=86400' });
+            res.end(content);
+        }
+    } else if (clearUrl == "/api/register") {
+        
+    } else {
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(sendResponse(false, null, "Not Found"));
+    }
+});
+
+const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: '/ws'
+})
+
+
+httpServer.listen(config.port, () => {
+    console.log('Server is running on http://localhost:' + config.port);
+    console.log(`Secure chats server V${VER}`)
+});
