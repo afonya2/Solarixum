6952702b06d0d538e1c64e64dd1f61453ef7b7f7
afonya <adamhir@freemail.hu>
2025-06-19 18:35:29 +0200

    begin new client

---
+++ clientdev/src/components/Login.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+    import { InputText, Button } from 'primevue';
+</script>
+
+<template>
+    <div class="bg">
+        <main class="w-full h-full md:w-50px">
+            <div class="flex flex-col gap-2">
+                <label for="username">Username</label>
+                <InputText type="text" placeholder="Username" id="username" style="margin-block: 5px;" />
+                <label for="password">Password</label>
+                <InputText type="password" placeholder="Password" id="password" style="margin-block: 5px;" />
+                <a href="javascript:alert('skill issue')">Forgot your password?</a>
+                <Button style="float: right;margin-top: 5px;">Log in</Button>
+            </div>
+        </main>
+    </div>
+</template>
+
+<style scoped>
+.bg {
+    background-image: url('../assets/loginbackground.jpg');
+    background-size: cover;
+    background-position: center;
+    height: 100vh;
+    width: 100%;
+}
+main {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: rgba(255, 255, 255, 0.5);
+    border-radius: 10px;
+    backdrop-filter: blur(10px) saturate(150%);
+    padding-inline: auto;
+    padding: 25px;
+    color: black;
+}
+input {
+    background-color: rgba(0,0,0,.75);
+}
+@media (width >= 48rem) {
+    main {
+        min-width: fit-content;
+        width: 25%;
+        height: fit-content;
+    }
+}
+a {
+    color: rgb(50, 100, 200);
+    text-decoration: underline;
+}
+</style>
---
+++ clientdev/src/main.ts
@@ -0,0 +1,46 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura'
+import { definePreset } from '@primeuix/themes';
+import Login from './components/Login.vue';
+
+const Preset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '#99b2e4',
+            100: '#84a2de',
+            200: '#7093d9',
+            300: '#5b83d3',
+            400: '#4774ce',
+            500: '#3264c8',
+            600: '#2850a0',
+            700: '#23468c',
+            800: '#193264',
+            900: '#0f1e3c',
+            950: '#0a1428'
+        },
+        secondary: {
+            50: '{emerald.50}',
+            100: '{emerald.100}',
+            200: '{emerald.200}',
+            300: '{emerald.300}',
+            400: '{emerald.400}',
+            500: '{emerald.500}',
+            600: '{emerald.600}',
+            700: '{emerald.700}',
+            800: '{emerald.800}',
+            900: '{emerald.900}',
+            950: '{emerald.950}'
+        }
+    }
+});
+
+let app = createApp(Login);
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Preset
+    }
+}).mount('#app')
